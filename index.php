6821e8ed106491df15854452646f3c757703bb06
radhigulati <radhigulati@gmail.com>
2020-02-06 11:32:18 -0700

    adding files for demo

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include_once("index.html"); 
+/*
+include (or require) statement takes all the text/code/markup that exists in the specified 
+file and copies it into the file that uses the include statement.
+*/
+?>
